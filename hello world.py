65cd10a59430b24bf697a5decd20bcddffef090a
CatworldLee <44438233+CatworldLee@users.noreply.github.com>
2021-05-06 07:29:06 +0800

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,6 @@
+import cv2
+
+file = cv2.imread("C:/Users/LiChengHao/Desktop/major/project/lab1-neural-networks-master/letters_mod/02.jpg")
+cv2.imshow('img',file)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
